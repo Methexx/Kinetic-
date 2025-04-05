32a3911b1570289eea62f551d8da87e8053576d0
Methexx <Methum@1234>
2025-04-06 03:48:13 +0530

    Feat:Create linear theme

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:kinetic/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(KineticApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
